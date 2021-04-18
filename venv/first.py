8a0652a910e86d638075c3a560eef62cb698000d
Hrushikar <hrushikarteja.k@gmail.com>
2021-04-18 21:05:39 +0530

    Update = Day 2

--- venv/first.py
+++ venv/first.py
@@ -1,5 +1,15 @@
+# Day 1
 print('Hello Python!! This is your programmer!!\n')
 a = int(input("Enter a number to display: "))
 print('Entered number (a): ', end='')
 print(a, end='\n\n')
 print('Good Bye!!')
+
+# Day 2
+sum = 0
+print('Enter the number of numbers to find their sum:')
+n = int(input())
+print('Enter the numbers to find their sum:')
+for i in range(n):
+    sum += int(input())
+print('Sum: '+str(sum))
