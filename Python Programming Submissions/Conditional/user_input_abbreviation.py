e583fbab599af043d5ae615320395b0a8f451496
Hrushikar <hrushikarteja.k@gmail.com>
2021-04-24 12:09:41 +0530

    Update - Python Programming Submissions

---
+++ Python Programming Submissions/Conditional/user_input_abbreviation.py
@@ -0,0 +1,9 @@
+string = input('Enter the abbreviation: ')
+if string == 'lol' or string == 'LOL' or string == 'LoL':
+    print('Laughing out loud')
+elif string == 'rofl' or string == 'ROFL' or string == 'RoFL':
+    print('Rolling on the floor laughing')
+elif string == 'lmk' or string == 'LMK':
+    print('Let me know')
+elif string == 'smh' or string == 'SMH':
+    print('Shaking my head')
